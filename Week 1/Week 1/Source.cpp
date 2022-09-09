0252f5e296df890b5891dfb1a63c7ec44cf3a388
Niels de Gruijl <niels07@outlook.com>
2022-09-09 17:05:11 +0200

    Non array and non array loop done

---
+++ Week 1/Week 1/Source.cpp
@@ -0,0 +1,125 @@
+#include <iostream>;
+#include <cstdio>;
+
+//============Non-Array Questions
+
+//int main()
+//{
+//	int a;
+//	 
+//	printf("Enter value for a: ");
+//	scanf_s("%d", &a);
+//
+//	if (a % 2 != 0)
+//	{
+//		printf("%d is an even number\n", a);
+//	}
+//  else
+//  {
+//		printf("%d is an odd number\n", a);
+//	}
+//	
+//	//===========================
+//
+//	int b, c;
+//	printf("Enter values for b and c respectively: ");
+//	scanf_s("%d %d", &b, &c);
+//	
+//	if (b > c)
+//	{
+//		printf("The highest value is %d\n", b);
+//	}
+//	else
+//	{
+//		printf("Highest value is %d\n", c);
+//	}
+//
+//	//===========================
+//
+//	int d, e, f;
+//	printf("Enter values for d, e and f respectively: ");
+//	scanf_s("%d %d %d", &d, &e, &f);
+//	printf("The average value is: %d\n", (d + e + f) / 3);
+//
+//	//===========================
+//
+//	int g, h, i, j;
+//	printf("Enter values for g, h, i and j respectively: ");
+//	scanf_s("%d %d %d %d", &g, &h, &i, &j);
+//	printf("The summation of these numbers is %d\n", g + h + i + j);
+//
+//	return 0;
+//}
+
+//============Non-Array Loop Questions
+
+//int main()
+//{
+//	int a;
+//	printf("Enter value for a: ");
+//	scanf_s("%d", &a);
+//
+//	for (int i = 0; i < a; i++)
+//	{
+//		printf("Hello World\n");
+//	}
+//
+//	//===========================
+//
+//	int b, c;
+//	printf("Enter values for b and c respectively: ");
+//	scanf_s("%d %d", &b, &c);
+//
+//	for (int i = 0; i < c; i++)
+//	{
+//		printf("%d\n", b);
+//	}
+//
+//	//===========================
+//
+//	int d;
+//	printf("Enter value for d: ");
+//	scanf_s("%d", &d);
+//
+//	if (d == 2 || d == 3)
+//		printf("%d is a prime number\n", d);
+//
+//	if (d <= 1 || d % 2 == 0 || d % 3 == 0)
+//		return 0;
+//
+//	for (int i = 5; i * i <= d; i += 6)
+//	{
+//		if (d % i == 0 || d % (i + 2) == 0)
+//			return 0;
+//	}
+//
+//	printf("%d is a prime number\n", d);
+//
+//	//===========================
+//
+//	int e;
+//	printf("Enter value for e: ");
+//	scanf_s("%d", &e);
+//	
+//	for (int i = e - 1; i > 0; i--)
+//	{
+//		e *= i;
+//		
+//		printf("%d\n", e);
+//	}
+//
+//	printf("Factorial of e is %d\n", e);
+//
+//	return 0;
+//}
+
+//============Array questions
+
+int main()
+{
+
+
+	return 0;
+}
+
+	
