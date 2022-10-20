5f4eedac0fa01f527ded4ed3225d84f9339a7c9d
Niels de Gruijl <niels07@outlook.com>
2022-10-20 17:40:09 +0200

    asd

--- Assignment/BattleGame/BattleGame/Main.cpp
+++ Assignment/BattleGame/BattleGame/Main.cpp
@@ -5,20 +5,9 @@
 #include "sceneHandler.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML with Scenes!");
-    Scene scene1("scene01");
-
-    SpriteObject sprite1("soldierSprite", "head.png");
-    sprite1.setPosition(sf::Vector2f(70.0f, 70.0f));
-    sprite1.setScale(sf::Vector2f(5.0f, 5.0f));
-    scene1.addGameObject(sprite1);
-
-    Scene scene2("scene02");
-
-    SpriteObject sprite2("soldierSprite", "head_hurt.png");
-    sprite2.setPosition(sf::Vector2f(70.0f, 70.0f));
-    sprite2.setScale(sf::Vector2f(5.0f, 5.0f));
-    scene2.addGameObject(sprite2);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "ShitShowdown");
+    Scene scene1("Main_Menu");
+    Scene scene2("Fight_Scene");
 
     SceneHandler handler;
     handler.addScene(scene1);
@@ -30,17 +19,6 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-            }
-            if (event.type == sf::Event::KeyReleased) {
-                if(event.key.code == sf::Keyboard::A) {
-                    if(counter == 0) {
-                        handler.stackScene("scene02");
-                        counter++;
-                    } else {
-                        handler.popScene();
-                        counter--;
-                    }
-                }
             }
         }
         window.clear();
---
+++ Assignment/BattleGame/BattleGame/Sprite.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "gameObject.hpp"
+#include <SFML/Graphics/Texture.hpp>
+
+class Sprite : public GameObject
+{
+public:
+	Sprite();
+	~Sprite();
+
+private:
+
+};
